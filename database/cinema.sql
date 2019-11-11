d9876d68b46a973e3784d064de13829733dfa890
arijec <adis.tulusak@gmail.com>
2019-11-11 09:29:07 +0100

    Database update - auto-generate ID

--- database/cinema.sql
+++ database/cinema.sql
@@ -2,14 +2,18 @@
 -- Database: cinema.sql
 
 -- Drop tables if they already exists
-DROP TABLE IF EXISTS `user`;
-DROP TABLE IF EXISTS `cultural_piece_of_work`;
 DROP TABLE IF EXISTS `user_attended`;
+DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `stars_in`;
+DROP TABLE IF EXISTS `seat`;
+DROP TABLE IF EXISTS `performer`;
 DROP TABLE IF EXISTS `cultural_event`;
 DROP TABLE IF EXISTS `hall`;
-DROP TABLE IF EXISTS `perfomer`;
-DROP TABLE IF EXISTS `seat`;
-DROP TABLE IF EXISTS `stars_in`;
+DROP TABLE IF EXISTS `cultural_piece_of_work`;
+
+
+
+
 
 -- Table structure for user
 CREATE TABLE `user`(
@@ -26,12 +30,12 @@ CREATE TABLE `user`(
 -- Table for all the movies given user saw
 CREATE TABLE `user_attended`(
     `username` VARCHAR(255) NOT NULL,
-    `id_piece_of_work` VARCHAR(255) NOT NULL
+    `id_piece_of_work` int(8) NOT NULL
 ) ENGINE=InnoDB CHARSET=utf8;
 
 -- Table structure for performance
 CREATE TABLE `cultural_piece_of_work`(
-    `id_piece_of_work` VARCHAR(255) NOT NULL,
+    `id_piece_of_work` int(8) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     `genre` VARCHAR(255) NOT NULL,
     `type` VARCHAR(255) NOT NULL, -- should it be enum or just varchar?
@@ -43,11 +47,11 @@ CREATE TABLE `cultural_piece_of_work`(
 
 -- Table structure event
 CREATE TABLE `cultural_event`(
-    `id_cultural_event` VARCHAR(255) NOT NULL,
+    `id_cultural_event` int(8) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `date` DATE NOT NULL,
     `time` TIME NOT NULL,
     `price` FLOAT(4, 2) NOT NULL,
-    `id_piece_of_work` VARCHAR(255) NOT NULL,
+    `id_piece_of_work` int(8) NOT NULL,
     `hall_num` VARCHAR(10) NOT NULL
 ) ENGINE=InnoDB CHARSET=utf8;
 
@@ -61,14 +65,14 @@ CREATE TABLE `hall`(
 
 -- Table structure for performer
 CREATE TABLE `performer`(
-    `performer_id` VARCHAR(255) NOT NULL,
+    `performer_id` int(8) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     `surname` VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB CHARSET=utf8;
 
 -- Table structure for seat
 CREATE TABLE `seat`(
-    `cultural_event_id` VARCHAR(255) NOT NULL,
+    `cultural_event_id` int(8) NOT NULL,
     -- we will see whether we need this or not
     -- `cultural_event_time` TIME NOT NULL, --why is this var name diffrent? than the one in cultural event?
     -- `cultural_event_date` DATE NOT NULL,
@@ -80,8 +84,8 @@ CREATE TABLE `seat`(
 -- Table for connesting stars that star in a piece of work
 CREATE TABLE `stars_in`(
     `stars_in_id` VARCHAR(255) NOT NULL,
-    `performer_id` VARCHAR(255) NOT NULL,
-    `id_piece_of_work` VARCHAR(255) NOT NULL
+    `performer_id` int(8) NOT NULL,
+    `id_piece_of_work` int(8) NOT NULL
 ) ENGINE=InnoDB CHARSET=utf8;
 
 -- Constraints for primary keys
@@ -91,17 +95,11 @@ ADD PRIMARY KEY(`username`);
 ALTER TABLE `user_attended`
 ADD PRIMARY KEY(`username`, `id_piece_of_work`);
 
-ALTER TABLE `cultural_piece_of_work`
-ADD PRIMARY KEY(`id_piece_of_work`);
-
-ALTER TABLE `cultural_event`
-ADD PRIMARY KEY(`id_cultural_event`);
-
 ALTER TABLE `hall`
 ADD PRIMARY KEY(`hall_num`);
 
-ALTER TABLE `performer`
-ADD PRIMARY KEY(`performer_id`);
+-- ALTER TABLE `performer`
+-- ADD PRIMARY KEY(`performer_id`);
 
 ALTER TABLE `seat`
 ADD PRIMARY KEY(`cultural_event_id`, `row`, `column`);
@@ -145,22 +143,36 @@ ADD CONSTRAINT FK_work_work
 FOREIGN KEY(`id_piece_of_work`) REFERENCES `cultural_piece_of_work`(`id_piece_of_work`)
 ON DELETE CASCADE;
 
+
+-- Trigger na generovanie ID
+-- CREATE SEQUENCE performer_ID_seq
+--   START WITH 1
+--   INCREMENT BY 1;
+
+-- CREATE TRIGGER performer_ID_gen
+--   before INSERT ON perfomer
+--   FOR EACH ROW
+--   BEGIN
+
+--   END;
+
+
 -- INSERT TO USER
 INSERT INTO `user` 
 VALUES 
     ('admin','Jožko', 'Mrkvicka', '1989-01-11','0903456789','admin@admin.com','admin', '$2y$10$4YH5Pyou8dsyCIHT0zbcEuReuZ86WAPSTUS0OvJB1Mc0.zNXA481q'), -- HESLO: admin
-    ('red','Robert', 'Mrkvicka', '1979-05-10','0903456987','red@gmail.com','redactor', 'ahoj'),
-    ('dolar','Ján', 'Mečiar', '1999-03-27','0903456789','admin@admin.com','cashier', 'fiko'),
-    ('Maniac42','Adela', 'Ostrolúcka', '1889-08-03','0903456789','admin@admin.com','viewer', 'ahoj'),
-    ('user','Jozko', 'Mrkvicka', '1989-11-01','0903456789','admin@admin.com','viewer', 'ahoj');
+    ('red','Robert', 'Mrkvicka', '1979-05-10','0903456987','red@gmail.com','redactor', '$2y$10$rdwJ8T1z7.GB2p3AOFlc4eR1qAHqTkeY4fwB6Z2xT/s262lguovL6'), -- HESLO: ahoj
+    ('dolar','Ján', 'Mečiar', '1999-03-27','0903456789','admin@admin.com','cashier', '$2y$10$8Z/97BmOjIIel9yTqUW/kO/MeHxeKtU8gxdQEuiQqRfuejCadYG2K'),   -- HESLO: fiko
+    ('Maniac42','Adela', 'Ostrolúcka', '1889-08-03','0903456789','admin@admin.com','viewer', '$2y$10$MRn3.qFHltGEasjknt0XeOYTGlniT4OVVqWdHr5T5tgi4REe.WVmy'), -- HESLO: ahoj
+    ('user','Jozko', 'Mrkvicka', '1989-11-01','0903456789','admin@admin.com','viewer', '$2y$10$hpN46XsemmBP35MoapSMZu4rC38Q8WUqgfytiWnZcsLi1AuqNGKHy'); -- HESL: ahoj
 
 -- INSERT TO cultural_piece_of_work
-INSERT INTO `cultural_piece_of_work`
+INSERT INTO `cultural_piece_of_work` (`name`, `genre`, `type`, `picture`, `description`, `duration`, `rating`)
 VALUES 
-    ('01234567899874561230', 'Deadpool','komédia','film','https://images-na.ssl-images-amazon.com/images/I/51Gh9OaWVcL.jpg','Než se stal Deadpoolem, byl Wadem Wilsonem (Ryan Reynolds), bývalým členem speciálních jednotek. Když mu lékaři diagnostikovali pokročilé stadium rakoviny, podrobil se experimentální léčbě v rámci programu Weapon X, známého a populárního především mezi x-menovskými mutanty. Díky tomu se Wade zbavil rakoviny, a nejenže zůstal naživu, ale také jako bonus získal schopnost rychlého samouzdravování. Bohužel však – například oproti Wolverinovi, který po podobné léčbě zůstal celkem sexy – z tohoto pokusu vyšel trvale znetvořený. ','108','81'),
-    ('01234567899874561242', 'Harry Potter a Tajomná komnata','dobrodružný','film','https://image.tmdb.org/t/p//w780//ygsu82q2YSrIdePnM2GLGjsFFjr.jpg','Harry Potter se po prázdninách vrací do Bradavic a nastupuje do druhého ročníku. A to i přes varování domácího skřítka Dobbyho, podle kterého mu v čarodějné škole hrozí smrt. Harry nedbá nářků skřítka působícího víc škody než užitku, ale potom se skutečně začnou dít podivné věci, na stěnách se objevují neznámé nápisy a několik studentů je přepadeno tajemným přízrakem. Co s tím má společného Tajemná komnata? ','161','77'),
-    ('01234567899874561SAA', 'Rýchlo a zbesilo','akčný','film','https://i.pinimg.com/564x/9f/8d/5a/9f8d5a00f1f8885dd6d4b8282e9273d5.jpg','Brianovi se konečně podaří stát se členem party vedené Dominicem, který je doslova blázen do nelegálních závodů, pořádaných většinou v noci na periférii Los Angeles. Je to obrovské divadlo pro obecenstvo zběsilých jízd, ale především posedlost pro piloty speciálně upravených superauťáků. S ohlušujícím řevem motorů, řítící se městem několikanásobkem povolené rychlosti, pění všem adrenalin v krvi. Ve hře jsou peníze, prestiž, obdiv dívek.','106','72'),
-    ('01234567899874561KMP', 'Star Wars: The Force Awakens','dráma','film','https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_.jpg','Luke Skywalker zmizel a galaxie zažívá temné časy. Postupně ji totiž začíná ovládat zločinecký První řád, který vzniknul na troskách Impéria. Proti Prvnímu řádu se vytvořil Odpor, do jehož čela se postavila princezna Leia. Oběma stranám je jasné, že pokud se jim podaří najít Luka Skywalkera dříve než jejich nepřátelům, vítězství bude jejich. Na planetě Jakku se právě vylodili vojáci Prvního řádu, takzvaní stormtroopeři, kteří zde hledají mapu cesty, jež má být klíčem k nalezení Luka.','136','76');
+    ('Deadpool','komédia','film','https://images-na.ssl-images-amazon.com/images/I/51Gh9OaWVcL.jpg','Než se stal Deadpoolem, byl Wadem Wilsonem (Ryan Reynolds), bývalým členem speciálních jednotek. Když mu lékaři diagnostikovali pokročilé stadium rakoviny, podrobil se experimentální léčbě v rámci programu Weapon X, známého a populárního především mezi x-menovskými mutanty. Díky tomu se Wade zbavil rakoviny, a nejenže zůstal naživu, ale také jako bonus získal schopnost rychlého samouzdravování. Bohužel však – například oproti Wolverinovi, který po podobné léčbě zůstal celkem sexy – z tohoto pokusu vyšel trvale znetvořený. ','108','81'),
+    ('Harry Potter a Tajomná komnata','dobrodružný','film','https://image.tmdb.org/t/p//w780//ygsu82q2YSrIdePnM2GLGjsFFjr.jpg','Harry Potter se po prázdninách vrací do Bradavic a nastupuje do druhého ročníku. A to i přes varování domácího skřítka Dobbyho, podle kterého mu v čarodějné škole hrozí smrt. Harry nedbá nářků skřítka působícího víc škody než užitku, ale potom se skutečně začnou dít podivné věci, na stěnách se objevují neznámé nápisy a několik studentů je přepadeno tajemným přízrakem. Co s tím má společného Tajemná komnata? ','161','77'),
+    ('Rýchlo a zbesilo','akčný','film','https://i.pinimg.com/564x/9f/8d/5a/9f8d5a00f1f8885dd6d4b8282e9273d5.jpg','Brianovi se konečně podaří stát se členem party vedené Dominicem, který je doslova blázen do nelegálních závodů, pořádaných většinou v noci na periférii Los Angeles. Je to obrovské divadlo pro obecenstvo zběsilých jízd, ale především posedlost pro piloty speciálně upravených superauťáků. S ohlušujícím řevem motorů, řítící se městem několikanásobkem povolené rychlosti, pění všem adrenalin v krvi. Ve hře jsou peníze, prestiž, obdiv dívek.','106','72'),
+    ('Star Wars: The Force Awakens','dráma','film','https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_.jpg','Luke Skywalker zmizel a galaxie zažívá temné časy. Postupně ji totiž začíná ovládat zločinecký První řád, který vzniknul na troskách Impéria. Proti Prvnímu řádu se vytvořil Odpor, do jehož čela se postavila princezna Leia. Oběma stranám je jasné, že pokud se jim podaří najít Luka Skywalkera dříve než jejich nepřátelům, vítězství bude jejich. Na planetě Jakku se právě vylodili vojáci Prvního řádu, takzvaní stormtroopeři, kteří zde hledají mapu cesty, jež má být klíčem k nalezení Luka.','136','76');
 
 -- INSERT TO hall 
 INSERT INTO `hall`
@@ -169,26 +181,29 @@ VALUES
     ('B','10','9','P.Sherman 43, Malaby bay, Sydney');
 
 -- INSERT TO cultural_event
-INSERT INTO `cultural_event`
+INSERT INTO `cultural_event` (`date`, `time`, `price`, `id_piece_of_work`, `hall_num`)
 VALUES 
-    ('PRODUCTA', '2019-11-01','10:00:00','4.99', '01234567899874561230', 'A'),
-    ('PRODUCTB', '2019-11-01','13:00:00','4.99', '01234567899874561230', 'B'),
-    ('PRODUCTC', '2019-09-01','10:00:00','4.99', '01234567899874561SAA', 'B');
+    ('2019-11-01','10:00:00','4.99', '1', 'A'),
+    ('2019-11-01','13:00:00','4.99', '2', 'B'),
+    ('2019-09-01','10:00:00','4.99', '2', 'B');
 
 -- INSERT TO performer
-INSERT INTO `performer`
+INSERT INTO `performer` (`name`, `surname`)
 VALUES
-    ('P1','John','Smith'),
-    ('P2','Stan','Lee');
+    ('John','Smith'),
+    ('Stan','Lee'),
+    ('Ryan', 'Raynolds');
 
 -- INSERT TO seat
 INSERT INTO `seat`
 VALUES
-    ('PRODUCTA', '1','1','available'),
-    ('PRODUCTA', '1','2','available');
+    ('1', '1','1','available'),
+    ('1', '1','2','available');
 
 -- relationship tables
 -- INSERT TO user_attended
 INSERT INTO `user_attended` 
 VALUES
-    ('dolar', '01234567899874561SAA');
+    ('dolar', '1');
+
+
